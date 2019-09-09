92f714f6c1b8233672b027d2b493ff7ff8d2f7df
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-1207.local>
2019-09-09 23:57:18 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dtacms.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
