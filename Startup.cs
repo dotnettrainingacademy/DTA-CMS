56cdbb77c5c32487ee2dfe9225eb4cd7aa45cf12
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-1207.local>
2020-08-07 11:24:55 +0530

    admin module

--- Startup.cs
+++ Startup.cs
@@ -30,14 +30,14 @@ namespace dtacms
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -65,6 +65,56 @@ namespace dtacms
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+
+            //CreateRolesUsers(serviceProvider).Wait();
         }
+
+        // Add couple of user - Admin ,Guest
+        private async Task CreateRolesUsers(IServiceProvider serviceProvider)
+        {
+            // UserManager, Rolemanager
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            // Intitalize roles
+            string[] roleNames = {"Admin", "Guest"};
+
+            IdentityResult roleResult;
+
+            foreach (var rolename in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(rolename);
+                if(!roleExist){
+                    // Create the role
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(rolename));
+                }
+            }
+
+            // Create User
+            IdentityUser user = await UserManager.FindByEmailAsync("admin@dta.com");
+
+            if(user == null)
+            {
+                user = new IdentityUser(){
+                    UserName = "Admin",
+                    Email = "admin@dta.com"
+                };
+                await UserManager.CreateAsync(user, "Admin@123");
+            }
+            await UserManager.AddToRoleAsync(user, roleNames[0]);
+
+            // Guest User
+            IdentityUser guest = await UserManager.FindByEmailAsync("guest@dta.com");
+
+            if(guest == null)
+            {
+                guest = new IdentityUser(){
+                    UserName = "Guest",
+                    Email = "guest@dta.com"
+                };
+                await UserManager.CreateAsync(guest, "Guest@123");
+            }
+            await UserManager.AddToRoleAsync(guest, roleNames[1]);
+        }    
     }
 }
